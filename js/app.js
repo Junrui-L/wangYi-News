4eb3edca4d8f078e0d958d004c7a616c4564f5ac
Lvjr317 <462293072@qq.com>
2016-08-29 10:38:49 +0800

    新增一个库

--- js/app.js
+++ js/app.js
@@ -5,25 +5,85 @@
 var app = angular.module('myApp', ['ionic', 'myApp.controller']);
 app.config(function ($ionicConfigProvider, $stateProvider, $urlRouterProvider) {
     /*不同平台的默认样式配置*/
-
-    $ionicConfigProvider.platform.ios.tabs.style('standard');
-    $ionicConfigProvider.platform.ios.tabs.position('bottom');
-    $ionicConfigProvider.platform.android.tabs.style('standard');
-    $ionicConfigProvider.platform.android.tabs.position('standard');
-
-    $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
-    $ionicConfigProvider.platform.android.navBar.alignTitle('left');
-
-    $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
-    $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
-
-    $ionicConfigProvider.platform.ios.views.transition('ios');
-    $ionicConfigProvider.platform.android.views.transition('android');
-    $ionicConfigProvider.backButton.previousTitleText(false);
+    //
+    //$ionicConfigProvider.platform.ios.tabs.style('standard');
+    //$ionicConfigProvider.platform.ios.tabs.position('bottom');
+    //$ionicConfigProvider.platform.android.tabs.style('standard');
+    //$ionicConfigProvider.platform.android.tabs.position('standard');
+    //
+    //$ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+    //$ionicConfigProvider.platform.android.navBar.alignTitle('left');
+    //
+    //$ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+    //$ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+    //
+    //$ionicConfigProvider.platform.ios.views.transition('ios');
+    //$ionicConfigProvider.platform.android.views.transition('android');
+    //$ionicConfigProvider.backButton.previousTitleText(false);
 
     /*配置路由*/
+    $stateProvider.state('tab', {
+            url: '/tab',
+            templateUrl: 'templates/tabs.html',
+            abstract: true
+        })
+        //新闻
+        .state('tab.news', {
+            url: '/news',
+            views: {
+                /*定义视图展示的页面*/
+                'tab-news': {
+                    templateUrl: 'templates/news.html',
+                    controller: 'newsController'
+                }
+            }
+        })
 
+        //
+        //.state('tab.news_content', {
+        //    url: '/news_content/:aid',
+        //    views: {
+        //        /*定义视图展示的页面*/
+        //        'tab-news': {
+        //            templateUrl: 'templates/news_content.html',
+        //            controller: 'newsContentController'
+        //        }
+        //    }
+        //})
+        .state('tab.lives', {
+            url: '/lives',
+            views: {
+                /*定义视图展示的页面*/
+                'tab-lives': {
+                    templateUrl: 'templates/lives.html',
+                    controller: 'livesController'
+                }
+            }
+        })
 
+        .state('tab.chats', {
+            url: '/chats',
+            views: {
+                /*定义视图展示的页面*/
+                'tab-chats': {
+                    templateUrl: 'templates/chats.html',
+                    controller: 'chatsController'
+                }
+            }
+        })
+
+        .state('tab.me', {
+            url: '/me',
+            views: {
+                /*定义视图展示的页面*/
+                'tab-me': {
+                    templateUrl: 'templates/me.html',
+                    controller: 'meController'
+                }
+            }
+        });
+    /*找不到路由跳转的默认*/
+    $urlRouterProvider.otherwise('/tab/news')
 
 
 });
--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,19 @@
 /**
  * Created by Administrator on 2016/8/27 0027.
  */
+angular.module('myApp.controller', [])
+    .controller('newsController', function ($scope, $rootScope, $state, $ionicSlideBoxDelegate) {
+
+
+    })
+    .controller('livesController', function ($scope, $rootScope, $state) {
+
+
+    })
+    .controller('chatsController', function ($scope, $rootScope, $state) {
+
+    })
+
+    .controller('meController', function ($scope, $rootScope, $state, forumServices) {
+
+    });
